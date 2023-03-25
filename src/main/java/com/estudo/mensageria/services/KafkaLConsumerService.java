a9560587af4b2a52a5b8e6c793e97b41ac9158d0
ec-ayrton <ec.ayrton@gmail.com>
2023-03-25 18:06:56 -0300

    Implementação do Ack manual para confirmar processamento das mensagens

--- src/main/java/com/estudo/mensageria/services/KafkaLConsumerService.java
+++ src/main/java/com/estudo/mensageria/services/KafkaLConsumerService.java
@@ -3,6 +3,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,16 +21,18 @@ public class KafkaLConsumerService {
 
     private static final Logger logger = LoggerFactory.getLogger(KafkaLConsumerService.class);
 
-    @KafkaListener(topics = "orders", groupId = "my-group")
-    public  void listenOrders(String message){
+    @KafkaListener(topics = "orders", groupId = "my-group", containerFactory = "kafkaListenerContainerFactory")
+    public  void listenOrders(@Payload String message,@Headers Acknowledgment ack){
         logger.info(RECEIVED_MESSAGE,message );
         orderService.saveOrderFromKafka(message);
+        ack.acknowledge();
     }
 
-    @KafkaListener(topics = "payments", groupId = "my-group")
-    public  void listenPayments(String message){
+    @KafkaListener(topics = "payments", groupId = "my-group", containerFactory = "kafkaListenerContainerFactory")
+    public  void listenPayments(@Payload String message,@Headers Acknowledgment ack){
         logger.info(RECEIVED_MESSAGE,message );
         paymentService.savePaymentFromKafka(message);
+        ack.acknowledge();
     }
 
 }
